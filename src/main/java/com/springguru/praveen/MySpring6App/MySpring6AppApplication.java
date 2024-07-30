efc97ad6dea3e56314902a85d3d15491b3ff36fd
praveenkumar09 <tpkprav@gmail.com>
2024-07-30 08:02:03 +0800

    Intitial commit

---
+++ src/main/java/com/springguru/praveen/MySpring6App/MySpring6AppApplication.java
@@ -0,0 +1,13 @@
+package com.springguru.praveen.MySpring6App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpring6AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpring6AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springguru/praveen/MySpring6App/MySpring6AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springguru.praveen.MySpring6App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpring6AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
