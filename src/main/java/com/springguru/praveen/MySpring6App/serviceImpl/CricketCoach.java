f9a62e3c10dddd6d7af0730d29e9865bc0e06e32
praveenkumar09 <tpkprav@gmail.com>
2024-08-02 06:09:27 +0800

    Added bean-lifecycle

--- src/main/java/com/springguru/praveen/MySpring6App/serviceImpl/CricketCoach.java
+++ src/main/java/com/springguru/praveen/MySpring6App/serviceImpl/CricketCoach.java
@@ -1,6 +1,8 @@
 package com.springguru.praveen.MySpring6App.serviceImpl;
 
 import com.springguru.praveen.MySpring6App.service.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
@@ -16,8 +18,18 @@ public class CricketCoach implements Coach {
         System.out.println("In constructor :"+getClass().getSimpleName());
     }
 
+    @PostConstruct
+    public void init() {
+        System.out.println("In init :"+getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("In preDestroy :"+getClass().getSimpleName());
     }
 }
